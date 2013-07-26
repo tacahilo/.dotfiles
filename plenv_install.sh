36cf9e07aa4999e69ba31ab5f9a7513b0ffb8bb0
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-07-26 12:43:40 +0900

    plenvインストールするスクリプト書いた

---
+++ plenv_install.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+if [ -d ~/.plenv ]; then
+  echo ~/.plenv already exists
+  exit 1
+fi
+
+cat <<'EOF'
+
+  _____           _        _ _ _             
+  \_   \_ __  ___| |_ __ _| | (_)_ __   __ _ 
+   / /\/ '_ \/ __| __/ _` | | | | '_ \ / _` |
+/\/ /_ | | | \__ \ || (_| | | | | | | | (_| |
+\____/ |_| |_|___/\__\__,_|_|_|_|_| |_|\__, |
+                                       |___/ 
+       _                                     
+ _ __ | | ___ _ ____   __                    
+| '_ \| |/ _ \ '_ \ \ / /                    
+| |_) | |  __/ | | \ V / _ _                 
+| .__/|_|\___|_| |_|\_(_|_|_)                
+|_|                                          
+
+EOF
+
+
+git clone git://github.com/tokuhirom/plenv.git ~/.plenv
+mkdir -p ~/.plenv/plugins
+git clone git://github.com/tokuhirom/Perl-Build.git ~/.plenv/plugins/perl-build
+
+cat <<'EOF'
+
+         ___  ___    __  __      
+__/\__  /   \/___\/\ \ \/__\_/\__
+\    / / /\ //  //  \/ /_\ \    /
+/_  _\/ /_// \_// /\  //__ /_  _\
+  \/ /___,'\___/\_\ \/\__/   \/  
+                                 
+
+EOF
