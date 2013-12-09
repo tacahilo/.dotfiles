ad2814b94a3026d3078ea1c56881ed396d465e27
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-12-10 04:38:01 +0900

    osxの設定を分離

--- setup.sh
+++ setup.sh
@@ -1,42 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+
+git clone --recursive https://github.com/Tacahilo/.vim.git $HOME/.vim
+git clone --recursive https://github.com/Tacahilo/.zsh.d.git $HOME/.zsh.d
 
 if expr "$OSTYPE" : "^darwin" >/dev/null; then
-    echo migrating...
-    cd ~/.
-else
-    exit 1
+  . setup.sh.osx
+lse
+  exit 1
 fi
-
-# install homebrew
-type -p brew >&/dev/null
-if [ $? -eq 0 ]; then
-    echo homebrew already installed
-else
-    echo install homebrew
-    ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
-    brew install git
-    brew update
-fi
-
-type -p brew >&/dev/null
-if [ $? -eq 0 ]; then
-    brew install tig
-    brew install mercurial
-    brew install zsh
-    brew install wget
-    brew install emacs --cocoa --HEAD --use-git-head
-    brew install vim
-    brew install macvim
-    brew install tree
-    brew install proctools
-    brew install fontforge
-    brew install gibo
-fi
-
-# vim
-git clone --recursive https://github.com/Tacahilo/.vim.git $HOME/.vim
-# zsh
-git clone --recursive https://github.com/Tacahilo/.zsh.d.git $HOME/.zsh.d
 
 # alias dotfiles
 for file in `find ~/.dotfiles -d 1 -name '.*' | grep -v '.git$' | sed "s/.*dotfiles\///g"`; do
