26693affefa5d4de4dc8e6a13f271e6c71549450
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-12-11 08:38:06 +0900

    dotfilesをcloneする

--- setup.sh
+++ setup.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-git clone --recursive https://github.com/Tacahilo/.vim.git $HOME/.vim
-git clone --recursive https://github.com/Tacahilo/.zsh.d.git $HOME/.zsh.d
+git clone --recursive git@github.com:Tacahilo/.dotfiles.git $HOME/.dotfiles
+git clone --recursive git@github.com:Tacahilo/.vim.git $HOME/.vim
+git clone --recursive git@github.com:Tacahilo/.zsh.d.git $HOME/.zsh.d
 
 if expr "$OSTYPE" : "^darwin" >/dev/null; then
   . setup.sh.osx
-lse
+else
   exit 1
 fi
-
-# alias dotfiles
-for file in `find ~/.dotfiles -d 1 -name '.*' | grep -v '.git$' | sed "s/.*dotfiles\///g"`; do
-   ln -s ~/.dotfiles/$file ~/$file
-done
