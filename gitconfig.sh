cfb86d372f0677ef36c20234e62f8bb5a7b73a23
Takahiro OKUMURA <hfm.garden@gmail.com>
2014-05-13 14:00:05 +0900

    add some aliases

--- gitconfig.sh
+++ gitconfig.sh
@@ -13,18 +13,25 @@ git config --global user.email "hfm.garden@gmail.com"
 echo set alias
 git config --global alias.a 'add -A'
 git config --global alias.ap 'add -p'
-git config --global alias.co checkout
-git config --global alias.ci 'commit --verbose'
-git config --global alias.ca 'commit --amend'
-git config --global alias.cl 'clone --recursive'
-git config --global alias.d diff
-git config --global alias.s 'status -sb'
-git config --global alias.r 'remote -v'
 git config --global alias.b 'branch -v'
+git config --global alias.ca 'commit --amend'
+git config --global alias.ci 'commit --verbose'
+git config --global alias.cl 'clone --recursive'
+git config --global alias.co checkout
+git config --global alias.d diff
+git config --global alias.dc 'diff --cached --ignore-space-change'
 git config --global alias.f 'fetch --prune'
 git config --global alias.l log
 git config --global alias.lg 'log --graph --all'
+git config --global alias.plo 'pull origin'
+git config --global alias.plom 'pull origin master'
+git config --global alias.po 'push origin'
+git config --global alias.pom 'push origin master'
+git config --global alias.ps 'push'
 git config --global alias.push 'push --verbose'
+git config --global alias.r 'remote -v'
+git config --global alias.re 'reset'
+git config --global alias.s 'status -sb'
 
 echo set editor vim
 git config --global core.editor vim
