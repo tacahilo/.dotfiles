9ecdd46bda487f57d808c633eaf616f5472013d8
Takahiro OKUMURA <hfm.garden@gmail.com>
2014-06-08 16:16:09 +0900

    home/bin and home/src

--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@
 git clone --recursive https://github.com/tacahilo/.dotfiles.git $HOME/.dotfiles
 git clone --recursive https://github.com/tacahilo/.vim.git $HOME/.vim
 git clone --recursive https://github.com/tacahilo/.zsh.d.git $HOME/.zsh.d
+ln -sf ~/.dotfiles/usr ~/usr
+mkdir ~/bin
+mkdir ~/src
 
 if expr "$OSTYPE" : "^darwin" >/dev/null; then
   . setup.sh.osx
