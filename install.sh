b9c5172972a59f41b0c5a8d31052d04ecc8bb391
Okumura Takahiro <hfm.garden@gmail.com>
2014-10-09 16:55:45 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ git clone --recursive https://github.com/tacahilo/.dotfiles.git $HOME/.dotfiles
 ln -sf $HOME/.dotfiles/.vim $HOME/.vim
 ln -sf $HOME/.dotfiles/.zsh.d $HOME/.zsh.d
 
-mkdir ~/{bin,src}
+mkdir -p ~/{bin,src}
 mkdir -p ~/usr/local/{bin,lib,src}
 
 if expr "$OSTYPE" : "^darwin" >/dev/null; then
