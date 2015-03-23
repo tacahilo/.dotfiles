4f6a9939e736882c119e5a1b274347428a323ae7
Takahiro OKUMURA <hfm.garden@gmail.com>
2015-03-24 02:09:34 +0900

    [add] anyenv

--- install.sh
+++ install.sh
@@ -7,9 +7,38 @@ else
     git clone --recursive https://github.com/tacahilo/.dotfiles.git $HOME/.dotfiles
 fi
 
-ln -sf $HOME/.dotfiles/.vim $HOME/.vim
-ln -sf $HOME/.dotfiles/.zsh.d $HOME/.zsh.d
-ln -sf $HOME/.dotfiles/.anyenv $HOME/.anyenv
+cat <<EOS
+###############
+# install vim #
+###############
+EOS
+ln -sf $HOME/.dotfiles/.vim $HOME
+curl -# https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
+vim +NeoBundleInstall +qall
+
+cat <<EOS
+###############
+# install zsh #
+###############
+EOS
+ln -sf $HOME/.dotfiles/.zsh.d $HOME
+
+cat <<EOS
+##################
+# install anyenv #
+##################
+EOS
+if [ -e "$HOME/.anyenv" ]; then
+    ln -sf $HOME/.dotfiles/.anyenv $HOME
+    mkdir -p $HOME/.anyenv/plugins
+    pushd $HOME/.anyenv/plugins
+    git clone https://github.com/znz/anyenv-update.git
+    git clone https://github.com/znz/anyenv-git.git
+    popd
+else
+    echo Already exists
+    ls -ld "$HOME/.anyenv"
+fi
 
 mkdir -p ~/{bin,src}
 mkdir -p ~/usr/local/{bin,lib,src}
