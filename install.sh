768b5628d5860772ceaebe0a22a1234affa510d1
Takahiro OKUMURA <hfm.garden@gmail.com>
2015-04-19 07:43:56 +0900

    [change] anyenvの設定追加

--- install.sh
+++ install.sh
@@ -1,91 +1,35 @@
 #!/bin/bash
 
-if [ -d "$HOME/.dotfiles" ]; then
-    echo Already exists
-    ls -ld "$HOME/.dotfiles"
-else
-    git clone --recursive https://github.com/tacahilo/.dotfiles.git $HOME/.dotfiles
-fi
+# clone dotfiles dir
+[ -d "$HOME/.dotfiles" ] || git clone --recursive https://github.com/tacahilo/.dotfiles.git $HOME/.dotfiles
 
-cat <<EOS
-###############
-# install vim #
-###############
-EOS
-ln -sf $HOME/.dotfiles/.vim $HOME
+# setting Vim
+ln -sf $HOME/.dotfiles/.vim $HOME/.vim
 curl --progress-bar https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
 vim +NeoBundleInstall +qall
 
-cat <<EOS
-###############
-# install zsh #
-###############
-EOS
+# zsh
 ln -sf $HOME/.dotfiles/.zsh.d $HOME
 
-cat <<EOS
-################
-# install *env #
-################
-EOS
-
-cat <<EOS
-#########
-# rbenv #
-#########
-EOS
-if [ ! -d "$HOME/.rbenv" ]; then
-    git clone --depth 1 https://github.com/sstephenson/rbenv.git $HOME/.rbenv
-    mkdir $HOME/.rbenv/plugins
-    git clone --depth 1 https://github.com/znz/rbenv-plug $HOME/.rbenv/plugins/rbenv-plug
+# anyenv
+if [ ! -d "$HOME/.anyenv" ]; then
+    git clone https://github.com/riywo/anyenv $HOME/.anyenv
+    mkdir -p $HOME/.anyenv/plugins && cd $HOME/.anyenv/plugins
+    git clone https://github.com/znz/anyenv-update.git
 fi
 
-plugins=(
-    ruby-build
-    rbenv-default-gems
-    rbenv-gem-rehash
-    rbenv-update
-    gem-src
-)
-for item in ${plugins[@]}; do
-  rbenv plug $item
-done
+PATH=$HOME/.anyenv/bin:$PATH
+eval "$(anyenv init -)"
 
-cat <<EOF > $HOME/.rbenv/default-gems
-artii
-awesome_print
-bundler
-debugger
-dotenv
-git-browse-remote
-hirb
-hirb-unicode
-ipaddress
-librarian-puppet
-mgem
-mysql2
-octokit
-pry
-pry-theme
-puppet
-puppet-lint
-reek
-rubocop
-sheet
-wirb
-EOF
+## *env by anyenv
+anyenv install rbenv
+mkdir -p $HOME/.anyenv/envs/rbenv/plugins && cd $HOME/.anyenv/envs/rbenv/plugins
+git clone https://github.com/amatsuda/gem-src.git
+git clone https://github.com/sstephenson/rbenv-gem-rehash.git
+git clone https://github.com/sstephenson/rbenv-default-gems.git
+ln -sf $HOME/.dotfiles/rbenv-default-gems $HOME/.anyenv/envs/rbenv/default-gems
 
-cat <<EOS
-#########
-# plenv #
-#########
-EOS
-if [ ! -d "$HOME/.plenv" ]; then
-    git clone --depth 1 git://github.com/tokuhirom/plenv.git ~/.plenv
-    mkdir ~/.plenv/plugins
-    git clone --depth 1 git://github.com/tokuhirom/Perl-Build.git ~/.plenv/plugins/perl-build
-    git clone --depth 1 git://github.com/Tacahilo/plenv-update.git ~/.plenv/plugins/plenv-update
-fi
+anyenv install plenv
 
 mkdir -p ~/{bin,src}
 mkdir -p ~/usr/local/{bin,lib,src}
