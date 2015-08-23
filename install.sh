3ef5614dab3d7a4f46bbddb281b1fe4a59a4a2a1
Takahiro OKUMURA <hfm.garden@gmail.com>
2015-08-24 01:06:05 +0900

    Install golang 1.5 and tools from golang

--- install.sh
+++ install.sh
@@ -40,3 +40,16 @@ mkdir -p ~/usr/local/{bin,lib,src}
 if expr "$OSTYPE" : "^darwin" >/dev/null; then
   . setup.sh.osx
 fi
+
+
+goenv install 1.5
+goenv global 1.5
+go get -u github.com/golang/lint
+go get -u github.com/mitchellh/gox
+go get -u github.com/motemen/ghq
+go get -u github.com/motemen/gore
+go get -u github.com/nsf/gocode
+go get -u github.com/spf13/hugo
+go get -u github.com/tcnksm/gcli
+go get -u golang.org/x/tools/cmd/goimports
+go get -u golang.org/x/tools/cmd/gorename
