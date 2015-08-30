fd8f05e9f405e67c1357ee76efc0308a4cfc6ee8
Takahiro OKUMURA <hfm.garden@gmail.com>
2015-08-30 17:19:43 +0900

    url

--- osx/cookbooks/vim/default.rb
+++ osx/cookbooks/vim/default.rb
@@ -6,7 +6,12 @@ git "#{Dir.home}/.vim" do
   repository "https://github.com/tacahilo/.vim"
 end
 
+def neobundle
+  "https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh"
+end
+
 execute "neobundle" do
-  command "curl -sS https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh"
+  command "curl -sS #{neobundle} | sh"
   not_if "test -d #{Dir.home}/.vim/bundle/neobundle.vim"
 end
+
