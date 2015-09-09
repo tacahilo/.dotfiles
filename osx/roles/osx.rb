7a47a45beabdd5004d7ad25598c0a39f3ed77cee
Takahiro OKUMURA <hfm.garden@gmail.com>
2015-09-09 23:46:30 +0900

    Add brew-cask

--- osx/roles/osx.rb
+++ osx/roles/osx.rb
@@ -1,3 +1,4 @@
+include_recipe "../cookbooks/homebrew_cask/default.rb"
 include_recipe "../cookbooks/browser/default.rb"
 include_recipe "../cookbooks/vim/default.rb"
 include_recipe "../cookbooks/zsh/default.rb"
