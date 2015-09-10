67313ac31c84aa155093c747bd23925db7317727
OKUMURA Takahiro <hfm.garden@gmail.com>
2015-09-10 11:39:55 +0900

    Install homebrew

--- osx/cookbooks/homebrew/default.rb
+++ osx/cookbooks/homebrew/default.rb
@@ -1,3 +1,8 @@
+BREW_URL = 'https://raw.githubusercontent.com/Homebrew/install/master/install'
+execute "ruby -e \"$(curl -fsSL #{BREW_URL})\"" do
+  not_if "which brew"
+end
+
 require 'itamae/plugin/resource/brew_tap'
 brew_tap 'caskroom/cask'
 package 'brew-cask'
