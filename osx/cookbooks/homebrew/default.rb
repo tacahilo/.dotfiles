b08160298b87778f46043ef8ed845c80aa63021c
OKUMURA Takahiro <hfm.garden@gmail.com>
2015-09-15 09:47:23 +0900

    Add terminal

--- osx/cookbooks/homebrew/default.rb
+++ osx/cookbooks/homebrew/default.rb
@@ -10,30 +10,18 @@ package 'brew-cask'
 %w(
   cmake
   cmigemo
-  colordiff
   ctags
-  direnv
   graphviz
   heroku-toolbelt
-  highlight
   imagemagick
   jpegoptim
   jq
-  less
-  lv
-  massren
   ncurses
   nkf
   openssl
   optipng
   pcre
-  reattach-to-user-namespace
   redis
-  ssh-copy-id
-  the_silver_searcher
-  tmux
-  tree
   urlview
-  watch
   weechat
 ).each { |pkg| package pkg }
---
+++ osx/cookbooks/terminal/default.rb
@@ -0,0 +1,25 @@
+include_recipe "../gnu/default.rb"
+include_recipe "../mail_client/default.rb"
+
+%w(
+  colordiff
+  direnv
+  highlight
+  jq
+  less
+  lv
+  massren
+  ncurses
+  reattach-to-user-namespace
+  ssh-copy-id
+  the_silver_searcher
+  tree
+  watch
+  weechat
+).each { |pkg| package pkg }
+
+package "tmux"
+remote_file "#{Dir.home}/.tmux.conf"
+
+package "urlview"
+remote_file "#{Dir.home}/.urlview"
